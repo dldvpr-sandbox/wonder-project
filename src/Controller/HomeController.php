c5e9e5b8752f815c236865d2117c1227329e4470
Ludovic <dormoy.dev@gmail.com>
2024-12-30 15:59:37 +0100

    some update form

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,8 +11,70 @@ class HomeController extends AbstractController
     #[Route('/', name: 'home')]
     public function index(): Response
     {
+        $questions = [
+            [
+                'title' => 'Je suis une question',
+                'content' => 'lorem ipsum dolor sit amet consectetur adipiscing elit pariatur',
+                'rating' => 20,
+                'author' => [
+                    'name' => 'John Doe',
+                    'avatar' => 'https://randomuser.me/api/portraits/lego/6.jpg',
+                ],
+                'nbrOfResponses' => 15,
+            ],
+            [
+                'title' => 'Je suis une question',
+                'content' => 'lorem ipsum dolor sit amet consectetur adipiscing elit pariatur',
+                'rating' => 15,
+                'author' => [
+                    'name' => 'John Doe',
+                    'avatar' => 'https://randomuser.me/api/portraits/lego/6.jpg',
+                ],
+                'nbrOfResponses' => 2,
+            ],
+            [
+                'title' => 'Je suis une question',
+                'content' => 'lorem ipsum dolor sit amet consectetur adipiscing elit pariatur',
+                'rating' => 0,
+                'author' => [
+                    'name' => 'John Doe',
+                    'avatar' => 'https://randomuser.me/api/portraits/lego/6.jpg',
+                ],
+                'nbrOfResponses' => 90,
+            ],
+            [
+                'title' => 'Je suis une question',
+                'content' => 'lorem ipsum dolor sit amet consectetur adipiscing elit pariatur',
+                'rating' => 45,
+                'author' => [
+                    'name' => 'John Doe',
+                    'avatar' => 'https://randomuser.me/api/portraits/lego/6.jpg',
+                ],
+                'nbrOfResponses' => 197,
+            ],
+            [
+                'title' => 'Je suis une question',
+                'content' => 'lorem ipsum dolor sit amet consectetur adipiscing elit pariatur',
+                'rating' => 4,
+                'author' => [
+                    'name' => 'John Doe',
+                    'avatar' => 'https://randomuser.me/api/portraits/lego/6.jpg',
+                ],
+                'nbrOfResponses' => 5,
+            ],
+            [
+                'title' => 'Je suis une question',
+                'content' => 'lorem ipsum dolor sit amet consectetur adipiscing elit pariatur',
+                'rating' => 23,
+                'author' => [
+                    'name' => 'John Doe',
+                    'avatar' => 'https://randomuser.me/api/portraits/lego/6.jpg',
+                ],
+                'nbrOfResponses' => 7,
+            ],
+        ];
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'questions' => $questions,
         ]);
     }
 }
--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -3,10 +3,10 @@
 namespace App\Controller;
 
 use App\Form\QuestionType;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/question', name: 'question_')]
 class QuestionController extends AbstractController
@@ -14,12 +14,13 @@ class QuestionController extends AbstractController
     #[Route('/ask', name: 'form')]
     public function index(Request $request): Response
     {
+
         $formQuestion = $this->createForm(QuestionType::class);
 
         $formQuestion->handleRequest($request);
 
         if ($formQuestion->isSubmitted() && $formQuestion->isValid()) {
-
+            dump($formQuestion->getData());
         }
 
         return $this->render('question/index.html.twig', [
