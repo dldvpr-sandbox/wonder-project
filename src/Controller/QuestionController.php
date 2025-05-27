7bb9a3e046c9f5c4f0bd2fa2c25f8255cbe3cd8f
Ludovic <dormoy.dev@gmail.com>
2025-05-27 12:36:12 +0200

    qustion form

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -5,7 +5,7 @@ namespace App\Controller;
 use App\Form\QuestionType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/question', name: 'question_')]
@@ -20,11 +20,31 @@ class QuestionController extends AbstractController
         $formQuestion->handleRequest($request);
 
         if ($formQuestion->isSubmitted() && $formQuestion->isValid()) {
-            dump($formQuestion->getData());
+            dd($formQuestion->getData());
         }
 
         return $this->render('question/index.html.twig', [
             'form' => $formQuestion->createView(),
+        ]);
+    }
+
+    #[Route('/{id}', name: 'show')]
+    public function show(Request $request, string $id): Response
+    {
+
+        $question =   [
+            'title' => 'Je suis une super question',
+            'content' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, adipisci. Libero aperiam dolores excepturi, quidem maxime accusantium inventore. Illum, odio dolores! Ullam omnis veritatis laborum, animi inventore nostrum optio voluptates.',
+            'rating' => 20,
+            'author' => [
+                'name' => 'Jean Dupont',
+                'avatar' => 'https://randomuser.me/api/portraits/men/52.jpg'
+            ],
+            'nbrOfResponse' => 15
+        ];
+
+        return $this->render('question/show.html.twig', [
+            'question' => $question,
         ]);
     }
 }
